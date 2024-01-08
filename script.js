d646e2d3c96d16f21e4b6bd304dbdf632ec8d6a3
cryptosamec <155995119+cryptosamec@users.noreply.github.com>
2024-01-08 18:12:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const form = document.getElementById("requestForm");
-const botToken = "<token>"; 
-const chatId = "<chat_id>";
+const botToken = "6912359694:AAFXf2PLVsXoBNMfZ7SWOlJQKTSIQDIF5II"; 
+const chatId = "-4089567138";
 
 form.addEventListener("submit", e => {
   e.preventDefault();
   
   let data = new FormData(form);
 
-  fetch(`https://api.telegram.org/bot${6912359694:AAFXf2PLVsXoBNMfZ7SWOlJQKTSIQDIF5II}/sendMessage`, {
+  fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
     method: "POST",
     body: new URLSearchParams({
-      chat_id: -4089567138,      
+      chat_id: chatId,      
       text: JSON.stringify(Object.fromEntries(data)) 
     })
   })
